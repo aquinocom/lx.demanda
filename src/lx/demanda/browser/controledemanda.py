f86d6619933abdf199b0cdb16685cf27e8e0bd15
andre.barbosa <andre.barbosa@tse.jus.br>
2015-10-15 14:07:47 -0300

    controle de HST

--- src/lx/demanda/browser/controledemanda.py
+++ src/lx/demanda/browser/controledemanda.py
@@ -16,10 +16,11 @@ from plone.memoize.instance import memoize
 from plone.registry.interfaces import IRegistry
 
 #Libs imports
+from DateTime import DateTime
 
 
 # lx.demanda imports
-from lx.demanda.interfaces.contents import IDemanda
+from lx.demanda.interfaces.contents import IDemanda, IAtividade
 from lx.demanda.interfaces.interfaces import ICatalogoServicoPrefsForm
 
 
@@ -34,18 +35,26 @@ class ControleDemandaView(BrowserView):
 
     def settings(self):
         if 'form.action.ordemServico' in self.request.form:
-            ordemServico = self.request.get('ordemServico', None)
-            if self.validateOrdemServico(ordemServico):
-                return self.getColaborador()
+            if self.validateOrdemServico():
+                return self.getColaboradores()
 
     @memoize
-    def validateOrdemServico(self, ordemServico):
+    def validateOrdemServico(self):
         """Validação
         """
         context = aq_inner(self.context)
         utils = getToolByName(context, 'plone_utils')
-        if (ordemServico == ' '):
+        ordemServico = self.request.get('ordemServico', None)
+        projetizada = self.request.get('projetizada', False)
+        start = self.request.get('start', None)
+        end = self.request.get('end', None)
+
+        if (ordemServico == ' ') and not(projetizada):
             self.errors['ordem_servico'] = "O campo é obrigatório."
+
+        if projetizada == 'True':
+            if (start == '') or (end == ''):
+                self.errors['projetizada'] = "Preencher as datas"
         # Check for errors
         if self.errors:
             utils.addPortalMessage("Corrija os erros.", type='error')
@@ -53,20 +62,31 @@ class ControleDemandaView(BrowserView):
         else:
             return True
 
+
     @memoize
-    def getColaborador(self):
+    def getColaboradores(self):
         """
         """
+        catalog = getToolByName(self, 'portal_catalog')
+        path_demandas = '/'.join(self.context.getPhysicalPath())
         ordemServico = self.request.get('ordemServico', None)
-        if (ordemServico != None) and (ordemServico != ' '):
-            catalog = getToolByName(self, 'portal_catalog')
-            path_demandas = '/'.join(self.context.getPhysicalPath())
-            demandas = catalog(portal_type="colaborador",
+        projetizada = self.request.get('projetizada', False)
+        if (ordemServico != ' ' and ordemServico != None) or projetizada:
+            colabs = catalog(portal_type="colaborador",
                                path=path_demandas,
                                sort_on='getObjPositionInParent',)
-            return demandas
 
+            list_colab = []
+            if colabs:
+                for colab in colabs:
+                    dic = {
+                        'nome': colab.Title,
+                        'url': colab.getURL(),
+                        'total': self.getTotalHST(colab),
+                    }
+                    list_colab.append(dic)
 
+            return list_colab
 
     @memoize
     def getDemandas(self, colab):
@@ -75,27 +95,72 @@ class ControleDemandaView(BrowserView):
         catalog = getToolByName(self, 'portal_catalog')
         path_demandas = colab.getPath()
         ordemServico = self.request.get('ordemServico', None)
+        projetizada = self.request.get('projetizada', False)
+        start = self.request.get('start', None)
+        end = self.request.get('end', None)
+
+        dic = {'demanda':[], 'atividades':[]}
+
         if (ordemServico != None) and (ordemServico != ' '):
             demandas = catalog(object_provides=IDemanda.__identifier__,
                                path=path_demandas,
                                ordem_servico=ordemServico,
-                               sort_on='Date',
+                               sort_on='chamado',
                                sort_order='reverse',)
-            return demandas
-        else:
-            demandas = catalog(object_provides=IDemanda.__identifier__,
-                               path=path_demandas,
-                               sort_on='Date',
-                               sort_order='reverse',)
-            return demandas
+
+            if demandas:
+                for i in demandas:
+                    dic['demanda'].append(i)
+
+        if projetizada == 'True':
+            if start and end:
+                atividades = self.getAtividades(colab)
+                if atividades:
+                    for i in atividades:
+                        dic['atividades'].append(i)
+
+        return dic
+
+    @memoize
+    def getAtividades(self, colab):
+        """
+        """
+        catalog = getToolByName(self, 'portal_catalog')
+        path_demandas = colab.getPath()
+        start = self.request.get('start', None)
+        end = self.request.get('end', None)
+
+        first_date = DateTime(start, datefmt='international')
+        last_date = DateTime(end + ' 23:59:59', datefmt='international')
+
+        atividades = catalog(object_provides=IAtividade.__identifier__,
+                           path=path_demandas,
+                           data_inicio={'query': first_date, 'range': 'min'},
+                           data_fim={'query': last_date, 'range': 'max'},
+                           sort_on='data_inicio')
+
+        return atividades
 
     @memoize
     def getTotalHST(self, colab):
         """
         """
-        demandas = self.getDemandas(colab)
+        tarefas = self.getDemandas(colab)
+        atividades = tarefas['atividades']
+        demandas = tarefas['demanda']
+
+        if not atividades and not demandas:
+            return '0'
+
+        if atividades and demandas:
+            lista_tarefas = atividades + demandas
+        if atividades and not demandas:
+            lista_tarefas = atividades
+        if not atividades and demandas:
+            lista_tarefas = demandas
+
         quantidades = []
-        for i in demandas:
+        for i in lista_tarefas:
             totalAt = i.quantHST
             quantidades.append(totalAt)
         total = sum(quantidades)
