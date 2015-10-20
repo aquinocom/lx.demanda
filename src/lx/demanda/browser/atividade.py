5655b29387d64b8fd35b23ec050ab405e34bd224
andre.barbosa <andre.barbosa@tse.jus.br>
2015-10-20 08:56:35 -0200

    Ajuste na view

--- src/lx/demanda/browser/atividade.py
+++ src/lx/demanda/browser/atividade.py
@@ -9,3 +9,18 @@ from Products.Five.browser import BrowserView
 class AtividadeView(BrowserView):
     """ view 
     """
+
+    def getData(self, data):
+        """
+        """
+        if data == 'inicio':
+            try:
+                date = self.context.data_inicio.strftime('%d/%m/%Y %H:%M')
+            except:
+                date = ''
+        if data == 'fim':
+            try:
+                date = self.context.data_fim.strftime('%d/%m/%Y %H:%M')
+            except:
+                date = ''
+        return date
